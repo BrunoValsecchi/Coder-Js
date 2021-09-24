ba5e9084c817144b9fff4c2b3c0351d7e8e7765d
BrunoValsecchi <cai.bruno.valsecchi@gmail.com>
2021-09-24 19:57:08 -0300

    incio JS

---
+++ app.js
@@ -0,0 +1,13 @@
+let nombrePersona = prompt("Ingrese su nombre")
+
+console.log(`Hola, ${nombrePersona}`)
+
+let numero = prompt("Ingrese un numero")
+let numero2 = 8
+console.log(parseInt(numero) + numero2)
+
+let añoNacimiento = prompt("Ingrese su año de nacimiento")
+
+let ciudadNatal = prompt("Ingrese su ciudad natal")
+
+alert(`Hola, soy ${nombrePersona} mi nacimiento fue en el año ${añoNacimiento} en la ciudad de ${ciudadNatal}`)  
